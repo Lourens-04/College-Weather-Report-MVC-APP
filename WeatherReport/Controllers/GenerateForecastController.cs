dc344c9ebec915975d8f0798d9b86e203e1aea8a
Lourens-04 <lourens.dp04@gmail.com>
2022-05-29 22:05:15 +0200

    Project Added

---
+++ WeatherReport/Controllers/GenerateForecastController.cs
@@ -0,0 +1,158 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WeatherReport.Models;
+using System.Collections;
+
+namespace WeatherReport.Controllers
+{
+    public class GenerateForecastController : Controller
+    {
+        private WeatherReportsEntities db = new WeatherReportsEntities();
+        //fields declared that will be used to generate a user specified weather forecast
+        //----------------------------------
+        static string [] cities;
+        static string startdate;
+        static string enddate;
+        //----------------------------------
+
+        //ActionResult for FavIndex where a user only see there favourite cities and then also can generate weather forecast
+        //--------------------------------------------------------------------------
+        public ActionResult FavIndex()
+        {
+            //Used if the view is opened and a user is not loged in
+            if (Session["User"] == null)
+            {
+                Session["loginck"] = "failed";
+                return RedirectToAction("Login", "LogInUsers");
+            }
+            else
+            {
+                //query to get a user favourites
+                var tt = db.favourite_Table.ToList().Where(x => x.Username.Equals(Session["User"]));
+                //puts all favourite in viewbag
+                ViewBag.City = new SelectList(tt, "City", "City");
+                return View();
+            }
+        }
+        //--------------------------------------------------------------------------
+
+        //ActionResult [HttpPost] to generate a user specified weather forecast for their favourites
+        //--------------------------------------------------------------------------
+        [HttpPost]
+        public ActionResult FavIndex(string [] City, string start_Date, string end_Date)
+        {
+            //Used if the view is opened and a user is not loged in
+            if (Session["User"] == null)
+            {
+                Session["loginck"] = "failed";
+                return RedirectToAction("Login", "LogInUsers");
+            }
+            else
+            {
+                var dd = db.Forecast_Table.ToList().Where(x => x.Date >= Convert.ToDateTime(start_Date.ToString())
+                && x.Date <= Convert.ToDateTime(end_Date.ToString()));
+                if (dd.Count() >= 1)
+                {
+                    //assigning user specified input to asign it to global variables
+                    cities = City;
+                    startdate = start_Date;
+                    enddate = end_Date;
+
+                    //go to display view
+                    return RedirectToAction("Display", "GenerateForecast");
+                }
+                else
+                {
+                    //if username is not in database
+                    ViewBag.Error = "There is no records for the date/dates selected";
+                    //query to get a user favourites
+                    var ff = db.favourite_Table.ToList().Where(x => x.Username.Equals(Session["User"]));
+                    //puts all favourite in viewbag
+                    ViewBag.City = new SelectList(ff, "City", "City");
+                }
+                return View();
+            }
+        }
+        //--------------------------------------------------------------------------
+
+        //ActionResult for FavIndex where a user see all cities in the database
+        //--------------------------------------------------------------------------
+        public ActionResult IndexAll()
+        {
+            //Used if the view is opened and a user is not loged in
+            if (Session["User"] == null)
+            {
+                Session["loginck"] = "failed";
+                return RedirectToAction("Login", "LogInUsers");
+            }
+            else
+            {
+                //puts all Cities in viewbag
+                ViewBag.City = new SelectList(db.City_Table, "City", "City");
+                return View();
+            }
+        }
+        //--------------------------------------------------------------------------
+
+        //ActionResult [HttpPost] to generate a user specified weather forecast for all cities
+        //--------------------------------------------------------------------------
+        [HttpPost]
+        public ActionResult IndexAll(string [] City, string start_Date, string end_Date)
+        {
+            //Used if the view is opened and a user is not loged in
+            if (Session["User"] == null)
+            {
+                Session["loginck"] = "failed";
+                return RedirectToAction("Login", "LogInUsers");
+            }
+            else
+            {
+                var dd = db.Forecast_Table.ToList().Where(x => x.Date >= Convert.ToDateTime(start_Date.ToString())
+                && x.Date <= Convert.ToDateTime(end_Date.ToString()));
+                if (dd.Count() >= 1)
+                {
+                    //assigning user specified input to asign it to global variables
+                    cities = City;
+                    startdate = start_Date;
+                    enddate = end_Date;
+
+                    //go to display view
+                    return RedirectToAction("Display", "GenerateForecast");
+                }
+                else
+                {
+                    //if username is not in database
+                    ViewBag.Error = "There is no records for the date/dates selected";
+                    //puts all Cities in viewbag
+                    ViewBag.City = new SelectList(db.City_Table, "City", "City");
+                }
+                return View();
+            }
+        }
+        //--------------------------------------------------------------------------
+
+        //Will go to the view to display all the specified results the user requested
+        //--------------------------------------------------------------------------
+        public ActionResult Display()
+        {
+            //Used if the view is opened and a user is not loged in
+            if (Session["User"] == null)//so the user cant skip between tabs
+            {
+                Session["loginck"] = "failed";
+                return RedirectToAction("Login", "LogInUsers");//redirect to a succes page
+            }
+            else
+            {
+                //query to get all the specified results the user requested
+                return View(db.Forecast_Table.ToList().
+                Where(x => x.Date >= Convert.ToDateTime(startdate.ToString()) 
+                && x.Date <= Convert.ToDateTime(enddate.ToString()) 
+                && cities.Contains(x.City)));
+            }
+        }
+        //--------------------------------------------------------------------------
+    }
+}
---
+++ WeatherReport/Controllers/LogInUsersController.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WeatherReport.Models;
+
+namespace WeatherReport.Controllers
+{
+    public class LogInUsersController : Controller
+    {
+        private WeatherReportsEntities db = new WeatherReportsEntities();
+
+        // GET: user
+        //-------------------------------------------------------------------------
+        public ActionResult Index()
+        {
+            return View();
+        }
+        //-------------------------------------------------------------------------
+
+        //ActionResult that will be used for the log in of a user
+        //-------------------------------------------------------------------------
+        public ActionResult Login()
+        {
+            //Used if the view is opened and a user is not loged in
+            if (Session["loginck"] as string == "failed")
+            {
+                ViewBag.Error = "Login First please";
+            }
+
+            return View();
+        }
+        //-------------------------------------------------------------------------
+
+        //ActionResult (HttpPost) that will be used for the log in of a user to check if the user is in the database
+        //-------------------------------------------------------------------------
+        [HttpPost]
+        public ActionResult login(string tbxUsername, string tbxPassword)
+        {
+            bool found = false;
+            //process the login
+            foreach (User_Table c in db.User_Table)//user is the table model  
+            {
+                //Gets username and password and assign it to variables
+                string dd = tbxPassword.GetHashCode().ToString();
+                string ss = dd;
+                //see if the username is in the database
+                if (c.Username.Equals(tbxUsername) && c.password.Equals(tbxPassword.GetHashCode().ToString()))
+                {
+                    //makes a seesion if the user is in the database
+                    Session["User"] = c.Username;// create session varible 
+                    found = true;
+                    //go strait to the users favourite page so they can view their forecast
+                    return RedirectToAction("FavIndex", "GenerateForecast");//redirect to a succes page
+                }
+            }
+            //if username is not in database
+            if (found == false)
+            {
+                //if username is Incorrect
+                ViewBag.Error = "Username or Password is Incorrect";
+            }
+            return View();
+        }
+        //-------------------------------------------------------------------------
+
+        //Sign out of the application
+        //-------------------------------------------------------------------------
+        public ActionResult SignOut()
+        {
+            Session["User"] = null;
+            return RedirectToAction("Index", "Home");
+        }
+        //-------------------------------------------------------------------------
+    }
+}
